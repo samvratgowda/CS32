c5f476820734c096de0920b7e209d032b0ee7898
samvratgowda <samvrat.gowda@gmail.com>
2026-01-11 21:58:23 -0800

    Clean submission: remove swap file and unnecessary helloWorld

--- lab00/helloWorld.cpp
+++
@@ -1,9 +0,0 @@
-// Edited by: Samvrat Gowda
-// minimal Hello World! program for testing Makefiles
-
-#include <iostream>
-
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
-}
