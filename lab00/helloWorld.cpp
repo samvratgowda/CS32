264f913d24d84ebcfad6f5edb9cb8a9a48d4043d
samvratgowda <samvrat.gowda@gmail.com>
2026-01-11 22:02:39 -0800

    Add compiled executables

---
+++ lab00/helloWorld.cpp
@@ -0,0 +1,9 @@
+// Edited by: Samvrat Gowda
+// minimal Hello World! program for testing Makefiles
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
